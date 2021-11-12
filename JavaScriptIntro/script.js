94a524914796c268a119125c41c3f9c6d980c05a
Daniel-Egorov <daniel.s.egorov@gmail.com>
2021-11-12 09:14:53 -0800

    mess around with Math.PI and Math.sin

--- JavaScriptIntro/script.js
+++ JavaScriptIntro/script.js
@@ -1,14 +1,32 @@
 
-// asks user for their name
-var name = prompt('What is your name?'); 
+
+var userName = prompt('What is your name?'); 
+
 // capitalize the first character and lowercase the rest, concatenate the two results
-name = name.toUpperCase().charAt(0) + name.toLowerCase().substring(1);
-// asks for age and immediately converts to int
+userName = userName.toUpperCase().charAt(0) + userName.toLowerCase().substring(1);
+
 var age = parseInt(prompt('How old are you?'));
-// asks for temp degrees F and immediately converts to int
+
 var temp = parseInt(prompt('What is the temperature? (Degrees F)'));
 
+var radius = parseFloat(prompt("What is the radius of your circle?"));
+
 // tells user how old they are in dog years, by multiplying age by 7
-alert(`${name}, you are ${age * 7} years old in dog years`);
+alert(`${userName}, you are ${age * 7} years old in dog years`);
 // tells user degrees F in degrees C by subtracting 32 from temp then multiplying by 5/9
-alert(`${name}, ${temp} degrees Fahrenheit is ${((temp - 32) * (5/9)).toFixed(2)} degrees Celsius`);
+alert(`${userName}, ${temp} degrees Fahrenheit is ${((temp - 32) * (5/9)).toFixed(2)} degrees Celsius`);
+// tells user area of circle given radius by doing pi * r^2
+alert(`${userName}, the area of your circle is ${(Math.PI * Math.pow(radius, 2)).toFixed(2)}`);
+
+var angle = parseFloat(prompt("What is the measurement of your angle?"));
+
+var sideLength = parseFloat(prompt("What is the length of the side opposite to your angle?"));
+
+var hypotenuse = (sideLength / Math.sin(angle)).toFixed(2);
+
+if (hypotenuse <= 0) {
+    alert(`${userName}, your triangle is invalid`);
+}
+else {
+    alert(`${userName}, the length of your hypotenuse is ${(sideLength / Math.sin(angle)).toFixed(2)}`);
+}
