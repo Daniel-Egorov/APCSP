bc1b9cab9d983d199494cdea353f130172cdd7a5
Daniel-Egorov <daniel.s.egorov@gmail.com>
2021-11-17 14:00:32 -0800

    show total games played and median score

--- GuessingGame-v1_0/script.js
+++ GuessingGame-v1_0/script.js
@@ -3,26 +3,38 @@
 // warm = 20
 // cold = 30
 // really cold = 40
+/*
+USER DATA JSON LAYOUT 
+{
+    "totalGames": 10,
+    "totalScore": 20,
+    "minScore": 3,
+    "allScores": [
+        1,
+        2,
+        3
+    ]
+}
+*/
 var userName = prompt("What is your name?");
 userName = userName.charAt(0).toUpperCase() + userName.substring(1).toLowerCase();
 alert(`Welcome, ${userName}!`);
-if (confirm(`${userName}, would you like to clear all your data?`)) localStorage.clear();
+
+// if (confirm(`${userName}, would you like to clear all your data?`)) localStorage.clear();
+
 var userData = JSON.parse(localStorage.getItem("userData"));
 
 confirmed = confirm('Would you like to play a game?');
 if (!userData) {
-    var totalGames = 0;
-    var totalScore = 0;
-    var lowestGuesses;
-}
-else {
-    var totalGames = userData.totalGames;
-    var totalScore = userData.totalScore;
-    var lowestGuesses = userData.lowestGuesses;
+    userData = {
+        "totalGames": 0,
+        "totalScore": 0,
+        "allScores": []
+    }
 }
 
 while (confirmed === true) {
-    totalGames++;
+    userData.totalGames++;
     var guessCount = 1;
     var rand = Math.floor(Math.random() * 100) + 1;
 
@@ -66,26 +78,59 @@ while (confirmed === true) {
         guessCount++;
     }
 
-    if (!lowestGuesses) {
-        lowestGuesses = guessCount;
+    if (!userData.minScore) {
+        userData.minScore = guessCount;
     }
-    else if (guessCount < lowestGuesses) {
-        lowestGuesses = guessCount;
+    else if (guessCount < userData.minScore) {
+        userData.minScore = guessCount;
     }
-    totalScore += guessCount;
+    userData.allScores.push(guessCount);
+    userData.totalScore += guessCount;
+
     confirmed = confirm(`${userName}, you guessed it!
-Your score was ${guessCount} guess(es).
-Your best score was ${lowestGuesses} guess(es).
+    Score: ${guessCount}
+    Best Score: ${userData.minScore}
+    Games Played: ${userData.allScores.length}
+    Avg Score: ${(userData.totalScore / userData.totalGames).toFixed(2)}
+    Median Score: ${median(userData.allScores)}
 Would you like to play again?`);
 }
 
-avgScore = totalScore / totalGames;
+console.log(userData.allScores);
 
-alert(`${userName}, Your average score is ${avgScore.toFixed(2)} guesses
-Have a good day!`);
+alert(`${userName}, Have a good day!`);
 
-localStorage.setItem('userData', JSON.stringify({
-    "totalGames": totalGames,
-    "totalScore": totalScore,
-    "lowestGuesses": lowestGuesses
-}));
+localStorage.setItem('userData', JSON.stringify(userData));
+
+function median(array) {
+    array = bubbleSort(array);
+    if (array.length % 2 === 0) {
+        let num1 = array[array.length / 2];
+        let num2 = array[(array.length / 2) - 1];
+        return ((num1 + num2) / 2).toFixed(2);
+    }
+    else {
+        return array[Math.floor(array.length / 2)];
+    }
+}
+
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+/*
+SAMPLE ARRAY
+[2, 4, 3, 8, 9, 10, 1]
+
+SAMPLE ARRAY 2
+[1, 7, 2, 4, 6, 3, 17, 4]
+*/
