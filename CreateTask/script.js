2a73915a1fd545e51ce51d23f6fb7e0e267a9021
Daniel-Egorov <daniel.s.egorov@gmail.com>
2021-12-01 14:19:29 -0800

    change comments for functions to JSDoc comments

--- CreateTask/script.js
+++ CreateTask/script.js
@@ -2,8 +2,13 @@ const rps = ["rock", "paper", "scissors"]; // used for computer to randomly choo
 const computerWins = []; // keep track of how many times computer wins with what object
 const userWins = []; // keep track of how many times user wins with what object
 
+/**
+ * Get occurrences of a value in an array
+ * @param {Array} array - any array to search through
+ * @param {*} value - value to search for in array
+ * @returns {int} amount - amount of times {value} appears in {array}
+ */
 function getOccurrences(array, value) {
-  // check how many times a value appears in an array
   amount = 0;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === value) {
@@ -13,14 +18,22 @@ function getOccurrences(array, value) {
   return amount;
 }
 
+/**
+ * Randomly choose object for computer
+ * @returns {String} "rock" "paper" or "scissors"
+ */
 function RPS() {
-  // computer randomly choose rock paper or scissors
   const rand = Math.floor(Math.random() * 3);
   return rps[rand];
 }
 
+/**
+ * Get the outcome of the game
+ * @param {String} user - what the user chose to play: "rock" "paper" or "scissors"
+ * @param {String} computer - what the computer chose to play: "rock" "paper" or "scissors"
+ * @returns result of the game: "lost" "won" or "tied"
+ */
 function userWin(user, computer) {
-  // check the outcome of the game based on the user's choice and computer's choice
   user = user.toLowerCase();
 
   if (user === computer) {
@@ -47,8 +60,10 @@ let computerStats = document.getElementById("computerStats"); // text to tell us
 let userInput = document.getElementById("textField"); // text field for user to input their choice
 let playButton = document.getElementById("playButton"); // button for user to confirm their choice
 
+/**
+ * Lines 67-80 run when the user clicks {playButton}
+ */
 playButton.addEventListener("click", function () {
-  // the code below runs when the user clicks the play button
   computer = RPS();
   let outcome = userWin(userInput.value, computer);
 
